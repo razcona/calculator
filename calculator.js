1f784c3f40e0124393e72c0f71848b1d65fb12c5
razcona <razcona3@gmail.com>
2024-03-31 22:09:13 -0400

    Modified the UI to look more like a basic calculator.
    Next step is to implement the logic of each button

--- calculator.js
+++ calculator.js
@@ -7,13 +7,13 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const modulo = (a, b) => a % b;
 
 function operate() {
     return operator === '+' ? add(firstNum, secondNum) 
     : operator === '-' ? subtract(firstNum, secondNum) 
     : operator === '*' ? multiply(firstNum, secondNum) 
-    : divide(firstNum, secondNum);
+    : operator === '/' ? divide(firstNum, secondNum) 
+    : modulo(firstNum, secondNum);
 }
-
-
 
