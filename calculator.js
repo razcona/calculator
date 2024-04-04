6c380cb76db580f7992275386e79b1b57162216b
razcona <razcona3@gmail.com>
2024-04-03 22:27:31 -0400

    Modified the script so that it successfully adds 2 numbers using the button UI

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
-let firstNum;
-let secondNum;
-let operator; 
+let firstNum = 0;
+let secondNum = 0;
+let operator = null; 
 let display = "";
 
 const add = (a, b) => a + b;
@@ -21,14 +21,41 @@ const output = document.querySelector("#display");
 const numbers = document.querySelector(".numbers");
 const operators = document.querySelector(".right");
 numbers.addEventListener("click", (e) => {
-    display += e.target.value;
-    output.innerHTML = display;
+    if (display.length < 11 && e.target.id != "equals") {
+        display += e.target.value;
+        output.innerHTML = display;
+    }
+
+    else if (e.target.id === "equals" && firstNum != 0 && operator != null) {
+        secondNum = Number(display);
+        display = operate().toString();
+        output.innerHTML = display;
+        console.log(firstNum);
+        console.log(secondNum);
+        console.log(operator);
+        console.log(display);
+    }
+
 });
 
 operators.addEventListener("click", (e) => {
     if(e.target.id === "clear") {
         display = "";
+        firstNum = 0;
+        secondNum = 0;
+        operator = null;
         output.innerHTML = "0";
-
     }
+
+    else if (e.target.id === "plus") {
+        operator = '+';
+        if (firstNum === 0) {
+            firstNum = Number(display);
+        } else { 
+            secondNum = Number(display);
+        }
+        display = "";
+        
+    }
+
 })
